e66c78d251745707a43376bb791a62b0cad84bc6
Roy Ge <rgecoder@gmail.com>
2020-02-29 14:38:26 -0800

    Add initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const numberOneInput = document.getElementById("numberOne");
+const numberTwoInput = document.getElementById("numberTwo");
+const addValuesButton = document.getElementById("addValues");
+const resultDiv = document.getElementById("result");
+const errorBox = document.getElementById("error");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const parseInputs = (...input) => {
+  return input.map(str => parseInt(str));
+};
 
-export default App;
+const inputsAreValid = (...input) => {
+  return input.every(num => typeof num === "number" && !isNaN(num));
+};
+
+const handleAdditionError = (inputs, numbers) => {
+  const fullMessage = inputs.reduce((message, str, index) => {
+    if (inputsAreValid(numbers[index])) {
+      return message + "";
+    } else {
+      return message + `${str} is not a number. `;
+    }
+  }, "Please enter two valid numbers! ");
+
+  errorBox.classList.remove("invisible");
+  errorBox.innerText = fullMessage;
+};
+
+const hideErrors = () => {
+  errorBox.classList.add("invisible");
+};
+
+hideErrors();
+addValuesButton.addEventListener("click", () => {
+  hideErrors();
+  const inputs = [numberOneInput.value, numberTwoInput.value];
+  const parsedInputs = parseInputs(...inputs);
+  if (inputsAreValid(...parsedInputs)) {
+    const [numA, numB] = parsedInputs;
+    resultDiv.innerText = numA + numB;
+  } else {
+    resultDiv.innerText = "";
+    handleAdditionError(inputs, parsedInputs);
+  }
+});
