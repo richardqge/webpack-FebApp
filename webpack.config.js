52c35de8bf728068d17273ca7d1b75e64fefa020
Roy Ge <rgecoder@gmail.com>
2020-03-01 06:14:45 -0800

    Add first loaders (style-loader css-loader) to handle CSS

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,14 @@ module.exports = {
     path: path.resolve(__dirname, "dist") //resolve absolute path to this CODE dir
     //so it's going to take w/e the current dir is, so for rge/.../CODE
     //and for someone else machine it would be colt/.../CODE
+  },
+  module:{
+    rules:[
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+
+      }
+    ]
   }
 };
