c03596d3ccbf61694ad09abb622a79f659802410
Roy Ge <rgecoder@gmail.com>
2020-03-01 07:05:51 -0800

    Add cache busting and HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 const path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development", 
   entry: "./src/index.js", //explicitly telling it now
   output: {
-    filename: "main.js",
+    filename: "main.[contentHash].js",
     path: path.resolve(__dirname, "dist") //resolve absolute path to this CODE dir
     //so it's going to take w/e the current dir is, so for rge/.../CODE
     //and for someone else machine it would be colt/.../CODE
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: "./src/template.html"
+  })],
   module:{
     rules:[
       {
